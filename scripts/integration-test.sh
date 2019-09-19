2fc46ba7412fb48214b1002f9ff19722b993650b
suskin <813571+suskin@users.noreply.github.com>
2019-09-19 16:34:11 -0700

    integration test: update integration test to use new features
    Now that the rest of the cli commands have been updated with the fixes
    and features for the new security model and cross-compatibility, we want
    to make sure the integration test uses the changes too.
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -9,38 +9,42 @@
 set -e
 set -x
 
-TEST_DIR="${1}"
+TEST_DIR="${1:-test}"
 
+mkdir -p "${TEST_DIR}"
 cd "${TEST_DIR}"
 
 GO111MODULE=on kubebuilder init --domain helloworld.stacks.crossplane.io
 yes | GO111MODULE=on kubebuilder create api --group samples --version v1alpha1 --kind HelloWorld
 
-kubectl crossplane stack init 'crossplane-examples/hello-world'
+kubectl crossplane stack init --cluster 'crossplane-examples/hello-world'
 
-sed -i .bak -e 's/\/\/ your logic here/r.Log.V(0).Info("Hello World!", "instance", req.NamespacedName)/' controllers/helloworld_controller.go
+sed -i'.bak' -e 's/\/\/ your logic here/r.Log.V(0).Info("Hello World!", "instance", req.NamespacedName)/' controllers/helloworld_controller.go
 rm -f controllers/helloworld_controller.go.bak
 
 GO111MODULE=on make manager manifests
 kubectl crossplane stack build local-build
-kubectl crossplane stack install 'crossplane-examples/hello-world' 'crossplane-examples-hello-world' localhost:5000
+kubectl crossplane stack install --cluster 'crossplane-examples/hello-world' 'crossplane-examples-hello-world' localhost:5000
 
-done=true
+finished='false'
+set +e
 for i in $( seq 1 10 ); do
   echo "Attempt ${i} to create resource . . ." >&2
-  kubectl apply -f config/samples/*.yaml || done=false
+  kubectl apply -f config/samples/*.yaml
 
-  if [[ "${done}" == false ]]; then
+  if [[ $? -ne 0 ]]; then
     echo "Create failed. Waiting before retry. . ." >&2
     sleep 2
     continue
   else
+    finished='true'
     echo "Create SUCCESS" >&2
     break
   fi
 done
+set -e
 
-if [[ "${done}" == false ]]; then
+if [[ "${finished}" == 'false' ]]; then
   echo "Error: Couldn't create resource after retries!" >&2
   exit 1
 fi
