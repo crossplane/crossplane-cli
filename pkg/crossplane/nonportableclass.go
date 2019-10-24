60876b7a492270d797a7e6348c65ddf7773c0d48
Hasan Turken <turkenh@gmail.com>
2019-10-25 00:11:40 +0300

    trace: fix finding related provider of a resource class
    Full object reference from resource class to provider is not available.
    Only name of the provider there. To resolve the provider, there was a
    workaround in the code, which sets apiVersion of the provider as the
    apiVersion of the resource class. This assumption does not hold with
    v1beta1 versions recently introduced in stack-gcp. This commit fixes
    by setting group instead of apiVersion.
    
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- pkg/crossplane/nonportableclass.go
+++ pkg/crossplane/nonportableclass.go
@@ -3,6 +3,8 @@ package crossplane
 import (
 	"strings"
 
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -41,22 +43,30 @@ func (o *NonPortableClass) GetObjectDetails() ObjectDetails {
 func (o *NonPortableClass) GetRelated(filterByLabel func(metav1.GroupVersionKind, string, string) ([]unstructured.Unstructured, error)) ([]*unstructured.Unstructured, error) {
 	related := make([]*unstructured.Unstructured, 0)
 	obj := o.instance.Object
+
+	// Provider ref
 	u, err := getObjRef(obj, fieldsNPCProvider)
 	if err != nil {
 		return related, err
 	}
-
 	if u != nil {
-		// TODO(hasan): Hack to set apiVersion for Provider until full object reference is available.
+		// TODO(hasan): Hack to find Provider until full object reference is available.
 		if u.GetAPIVersion() == "" {
-			oApiVersion := o.instance.GetAPIVersion()
-			s := strings.Split(oApiVersion, ".")
-			a := strings.Join(s[1:], ".")
-
-			u.SetAPIVersion(a)
-		}
-		if u.GetKind() == "" {
-			u.SetKind("Provider")
+			gv, err := schema.ParseGroupVersion(o.instance.GetAPIVersion())
+			if err != nil {
+				return related, err
+			}
+			s := strings.Split(gv.Group, ".")
+			g := strings.Join(s[1:], ".")
+			k := u.GetKind()
+			if k == "" {
+				k = "Provider"
+			}
+			u.SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   g,
+				Version: "",
+				Kind:    k,
+			})
 		}
 		related = append(related, u)
 	}
