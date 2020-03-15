c185848c40767beef221247cba61b2b71e37583d
hasheddan <georgedanielmangum@gmail.com>
2020-03-15 00:29:22 -0500

    Add crossplane apache 2 license
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- pkg/crossplane/claim.go
+++ pkg/crossplane/claim.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package crossplane
 
 import (
@@ -57,7 +73,7 @@ func (o *Claim) GetRelated(filterByLabel func(metav1.GroupVersionKind, string, s
 	if u != nil {
 		// TODO(hasan): Forward compatible hack for claim -> portableClass, currently apiversion, kind and ns missing
 		//  hence we need to manually fill them. This limitation will be removed with
-		//  https://github.com/crossplaneio/crossplane/blob/master/design/one-pager-simple-class-selection.md
+		//  https://github.com/crossplane/crossplane/blob/master/design/one-pager-simple-class-selection.md
 		if u.GetAPIVersion() == "" {
 			u.SetAPIVersion(o.instance.GetAPIVersion())
 			u.SetKind(o.instance.GetKind() + "Class")
